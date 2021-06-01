93b06529e7f47a5eb1e8e41f371123d830a08d93
Luan-vP <luanvanpletsen@gmail.com>
2021-06-01 08:50:25 +0100

    Add tests.py

---
+++ tests.py
@@ -0,0 +1,17 @@
+from parser import *
+
+print("Test preprocess() - 3 tests")
+
+test_sentences = ["I am 10 years old.", "My life is amazing", "Human beings can rise to all of our challenges"]
+answers = {}
+answers["I am 10 years old."] = ["i", "am", "years", "old"],
+answers["My life is amazing"] = ["my", "life", "is", "amazing"]
+answers["Human beings can rise to all of our challenges"] = ["human","beings","can","rise","to","all","of","our","challenges"]
+
+for test in test_sentences:
+    print("test:")
+    print(test)
+    if preprocess(test) == answers[test]:
+        print("Success!")
+    else:
+        print("Fail")
